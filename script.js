e268f5b6631758e9445226e904b32b6ef083b4db
Pradhum1 <113616448+Pradhum1@users.noreply.github.com>
2024-08-10 21:44:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let para = document.querySelector("#timer")
+function currentTime(){
+	let hour = date.getHours()
+	let min = date.getMinutes()
+	let sec = date.getSeconds()
+	let time = hour+":"+min+":"+sec
+	let day = date.getDay()
+	let month = date.getMonths()
+	let year = date.getFullYear()
+
+	para.innerHTML =`${month}/ ${day}/ ${year}, ${time}`
+}
+setInterval(currentTime,1000)
