72081b2ed858d3de83b92acef8e3a6c1d39357af
Pradhum1 <113616448+Pradhum1@users.noreply.github.com>
2024-08-10 22:29:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,42 @@
 //your JS code here. If required.
-let para = document.querySelector("#timer")
-function currentTime(){
-	let date = new Date()
-	let hour = date.getHours()
-	let min = date.getMinutes()
-	let sec = date.getSeconds()
-	let time = hour+":"+min+":"+sec
-	let day = date.getDay()
-	let month = date.getMonths()
-	let year = date.getFullYear()
-	let arrayOfTime = convertTime(hour, min, sec)
-	for(i=0; i<=2; i++){
-		arrayOfTime[i]=addZero(arrayOfTime[i])
-	}
-	let time = `${arrayOfTime[0]}:${arrayOfTime[1]}:${arrayOfTime[2]} ${arrayOfTime[3]}`
+let para = document.querySelector("#timer");
 
-	para.innerHTML =`${month}/ ${day}/ ${year}, ${time}`
+function currentTime(){
+    let date = new Date(); // Declare date object
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    let day = date.getDate(); // Use getDate() instead of getDay()
+    let month = date.getMonth() + 1; // Use getMonth() and add 1
+    let year = date.getFullYear();
+    let arrayOfTime = convertTime(hour, min, sec);
+    for(let i=0; i<=2; i++){
+        arrayOfTime[i]=addZero(arrayOfTime[i]);
+    }
+    let formattedTime = `${arrayOfTime[0]}:${arrayOfTime[1]}:${arrayOfTime[2]} ${arrayOfTime[3]}` // Rename the variable to avoid name clash
+
+    para.innerHTML =`${month}/ ${day}/ ${year}, ${formattedTime}`;
 }
-setInterval(currentTime,1000)
+
+setInterval(currentTime,1000);
 
 function convertTime(hr, min, sec){
-	if(hr>12){
-		hr=hr-12
-		return [hr,min,sec,"pm"]
-	}
-	else if(hr==0){
-		
-		return [12,min,sec,"AM"]
-	}
-	else if(hr<12){
-		
-		return [hr,min,sec,"AM"]
-	}
-	else if(hr==12){
-		
-		return [hr,min,sec,"pm"]
-	}
+    if(hr>12){
+        hr=hr-12;
+        return [hr,min,sec,"PM"];
+    } else if(hr==0){
+        return [12,min,sec,"AM"];
+    } else if(hr<12){
+        return [hr,min,sec,"AM"];
+    } else if(hr==12){
+        return [hr,min,sec,"PM"];
+    }
 }
 
 function addZero(k){
-	if(k<10){
-		return "0"+k
-	}
-	else{
-		return k
-	}
+    if(k<10){
+        return "0"+k;
+    } else {
+        return k;
+    }
 }
